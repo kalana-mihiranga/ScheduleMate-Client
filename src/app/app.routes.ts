135362e0c6142b67f0578baec2749c688dd68a9f
asiya99 <84146775+Asitha-Muthumala@users.noreply.github.com>
2024-10-18 22:41:02 +0530

    latest development

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { LandingComponent } from './page/common/landing/landing.component';
 import { LoginComponent } from './page/common/login/login.component';
 import { RegisterComponent } from './page/common/register/register.component';
+import { BusinessLandingComponent } from './page/business/business-landing/business-landing.component';
 
 export const routes: Routes = [
+    { path: 'businessLanding', component: BusinessLandingComponent },
     { path: 'home', component: LandingComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
