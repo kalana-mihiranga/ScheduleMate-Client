770840b79a72aa4f3d5c3c44be29cc33a7cf3148
asiya99 <84146775+Asitha-Muthumala@users.noreply.github.com>
2024-10-19 12:29:06 +0530

    package-page develop

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { LandingComponent } from './page/common/landing/landing.component';
 import { LoginComponent } from './page/common/login/login.component';
 import { RegisterComponent } from './page/common/register/register.component';
 import { BusinessLandingComponent } from './page/business/business-landing/business-landing.component';
+import { BusinessPackageComponent } from './page/business/business-package/business-package.component';
 
 export const routes: Routes = [
     { path: 'businessLanding', component: BusinessLandingComponent },
+    { path: 'businessPackage', component: BusinessPackageComponent },
     { path: 'home', component: LandingComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
