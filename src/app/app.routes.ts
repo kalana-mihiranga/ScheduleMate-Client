c04fd4f3b28a0003199127a61ccc2eae9f690f4c
asiya99 <84146775+Asitha-Muthumala@users.noreply.github.com>
2024-10-23 00:00:25 +0530

    latest changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,15 @@ import { RegisterComponent } from './page/common/register/register.component';
 import { BusinessLandingComponent } from './page/business/business-landing/business-landing.component';
 import { BusinessPackageComponent } from './page/business/business-package/business-package.component';
 import { BusinessTourComponent } from './page/business/business-tour/business-tour.component';
-import { BusinessPackageAddComponent } from './page/business/business-package-add/business-package-add.component';
+import { ClientLandingComponent } from './page/client/client-landing/client-landing.component';
+import { ServiceViewComponent } from './page/client/service-view/service-view.component';
 
 export const routes: Routes = [
     { path: 'businessLanding', component: BusinessLandingComponent },
     { path: 'businessPackage', component: BusinessPackageComponent },
-    { path: 'businessPackageAdd', component: BusinessPackageAddComponent },
     { path: 'businessTour', component: BusinessTourComponent },
+    { path: 'clientLanding', component: ClientLandingComponent },
+    { path: 'serviceView', component: ServiceViewComponent },
     { path: 'home', component: LandingComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
