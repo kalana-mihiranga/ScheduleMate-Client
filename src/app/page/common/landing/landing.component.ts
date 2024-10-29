2cf793635eb47ccc0698627b7a15923656f4cf58
asiya99 <84146775+Asitha-Muthumala@users.noreply.github.com>
2024-10-29 20:39:00 +0530

    filter modal feed

--- src/app/page/common/landing/landing.component.ts
+++ src/app/page/common/landing/landing.component.ts
@@ -1,98 +1,114 @@
-import { Component } from '@angular/core';
+import { Component, inject, TemplateRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from '../../../common/component/footer/footer.component';
 import { NavbarComponent } from '../../../common/component/navbar/navbar.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
   imports: [FooterComponent, NavbarComponent, CommonModule],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.scss'
+  styleUrl: './landing.component.scss',
 })
-export class LandingComponent {
 
+export class LandingComponent {
   feedItems: any[] = [
     {
       id: 10,
-      businessName: "Anya Boat Tours",
-      description: "Madu River Based Boat Safari",
-      location: "Ambalangoda, SL",
+      businessName: 'Anya Boat Tours',
+      description: 'Madu River Based Boat Safari',
+      location: 'Ambalangoda, SL',
       rating: 4.5,
-      imageUrl: "https://belfasthills.org/wp-content/uploads/2014/02/Sample-image-2-150x150.jpg"
+      imageUrl:
+        'https://belfasthills.org/wp-content/uploads/2014/02/Sample-image-2-150x150.jpg',
     },
     {
       id: 10,
-      businessName: "Anya Boat Tours",
-      description: "Madu River Based Boat Safari",
-      location: "Ambalangoda, SL",
+      businessName: 'Anya Boat Tours',
+      description: 'Madu River Based Boat Safari',
+      location: 'Ambalangoda, SL',
       rating: 4.5,
-      imageUrl: "https://belfasthills.org/wp-content/uploads/2014/02/Sample-image-2-150x150.jpg"
+      imageUrl:
+        'https://belfasthills.org/wp-content/uploads/2014/02/Sample-image-2-150x150.jpg',
     },
     {
       id: 10,
-      businessName: "Anya Boat Tours",
-      description: "Madu River Based Boat Safari",
-      location: "Ambalangoda, SL",
+      businessName: 'Anya Boat Tours',
+      description: 'Madu River Based Boat Safari',
+      location: 'Ambalangoda, SL',
       rating: 4.5,
-      imageUrl: "https://belfasthills.org/wp-content/uploads/2014/02/Sample-image-2-150x150.jpg"
+      imageUrl:
+        'https://belfasthills.org/wp-content/uploads/2014/02/Sample-image-2-150x150.jpg',
     },
     {
       id: 10,
-      businessName: "Anya Boat Tours",
-      description: "Madu River Based Boat Safari",
-      location: "Ambalangoda, SL",
+      businessName: 'Anya Boat Tours',
+      description: 'Madu River Based Boat Safari',
+      location: 'Ambalangoda, SL',
       rating: 4.5,
-      imageUrl: "https://belfasthills.org/wp-content/uploads/2014/02/Sample-image-2-150x150.jpg"
+      imageUrl:
+        'https://belfasthills.org/wp-content/uploads/2014/02/Sample-image-2-150x150.jpg',
     },
     {
       id: 10,
-      businessName: "Anya Boat Tours",
-      description: "Madu River Based Boat Safari",
-      location: "Ambalangoda, SL",
+      businessName: 'Anya Boat Tours',
+      description: 'Madu River Based Boat Safari',
+      location: 'Ambalangoda, SL',
       rating: 4.5,
-      imageUrl: "https://belfasthills.org/wp-content/uploads/2014/02/Sample-image-2-150x150.jpg"
+      imageUrl:
+        'https://belfasthills.org/wp-content/uploads/2014/02/Sample-image-2-150x150.jpg',
     },
     {
       id: 10,
-      businessName: "Anya Boat Tours",
-      description: "Madu River Based Boat Safari",
-      location: "Ambalangoda, SL",
+      businessName: 'Anya Boat Tours',
+      description: 'Madu River Based Boat Safari',
+      location: 'Ambalangoda, SL',
       rating: 4.5,
-      imageUrl: "https://belfasthills.org/wp-content/uploads/2014/02/Sample-image-2-150x150.jpg"
+      imageUrl:
+        'https://belfasthills.org/wp-content/uploads/2014/02/Sample-image-2-150x150.jpg',
     },
     {
       id: 10,
-      businessName: "Anya Boat Tours",
-      description: "Madu River Based Boat Safari",
-      location: "Ambalangoda, SL",
+      businessName: 'Anya Boat Tours',
+      description: 'Madu River Based Boat Safari',
+      location: 'Ambalangoda, SL',
       rating: 4.5,
-      imageUrl: "https://belfasthills.org/wp-content/uploads/2014/02/Sample-image-2-150x150.jpg"
+      imageUrl:
+        'https://belfasthills.org/wp-content/uploads/2014/02/Sample-image-2-150x150.jpg',
     },
     {
       id: 10,
-      businessName: "Anya Boat Tours",
-      description: "Madu River Based Boat Safari",
-      location: "Ambalangoda, SL",
+      businessName: 'Anya Boat Tours',
+      description: 'Madu River Based Boat Safari',
+      location: 'Ambalangoda, SL',
       rating: 4.5,
-      imageUrl: "https://belfasthills.org/wp-content/uploads/2014/02/Sample-image-2-150x150.jpg"
+      imageUrl:
+        'https://belfasthills.org/wp-content/uploads/2014/02/Sample-image-2-150x150.jpg',
     },
     {
       id: 10,
-      businessName: "Anya Boat Tours",
-      description: "Madu River Based Boat Safari",
-      location: "Ambalangoda, SL",
+      businessName: 'Anya Boat Tours',
+      description: 'Madu River Based Boat Safari',
+      location: 'Ambalangoda, SL',
       rating: 4.5,
-      imageUrl: "https://belfasthills.org/wp-content/uploads/2014/02/Sample-image-2-150x150.jpg"
+      imageUrl:
+        'https://belfasthills.org/wp-content/uploads/2014/02/Sample-image-2-150x150.jpg',
     },
     {
       id: 10,
-      businessName: "Anya Boat Tours",
-      description: "Madu River Based Boat Safari",
-      location: "Ambalangoda, SL",
+      businessName: 'Anya Boat Tours',
+      description: 'Madu River Based Boat Safari',
+      location: 'Ambalangoda, SL',
       rating: 4.5,
-      imageUrl: "https://belfasthills.org/wp-content/uploads/2014/02/Sample-image-2-150x150.jpg"
+      imageUrl:
+        'https://belfasthills.org/wp-content/uploads/2014/02/Sample-image-2-150x150.jpg',
     },
   ];
 
+  private modalService = inject(NgbModal);
+
+  openFilterFeedModal(content: TemplateRef<any>) {
+    this.modalService.open(content, { centered: true });
+  }
 }
